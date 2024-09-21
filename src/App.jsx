c30dda56063ff50b2a0956ca935e84f4c4d62cb7
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-09-21 11:33:05 +0600

    update the useEffect hook to print the sentence word by word

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import { MoonLoader } from 'react-spinners';
+import './App.css';
+import axios from 'axios';
 
 function App() {
   const [paragraphText, setParagraphText] = useState('');
+  const [loader, setLoader] = useState(false);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentSentence, setCurrentSentence] = useState('');
 
   useEffect(() => {
-    // Send a message to the content script to get the <p> tag text
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "getParagraphText" }, (response) => {
-        if (response && response.text) {
-          setParagraphText(response.text);
-        } else {
-          setParagraphText("No tag found or unable to retrieve content.");
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'getParagraphText' }, async (response) => {
+        try {
+          setLoader(true);
+          if (response && response.text) {
+            const response_data = await axios.post('http://127.0.0.1:8000/summarize', {
+              content: response.text,
+            });
+            setParagraphText(response_data.data.summary);
+          } else {
+            setParagraphText('No tag found or unable to retrieve content.');
+          }
+        } catch {
+          console.log('error');
+        } finally {
+          setLoader(false);
         }
       });
     });
   }, []);
 
+  const words = paragraphText.split(' ');
+
+  useEffect(() => {
+    if (paragraphText) {
+      const interval = setInterval(() => {
+        // Check if we haven't reached the end of the words array
+        if (currentWordIndex < words.length) {
+          setCurrentSentence((prev) => prev + ' ' + words[currentWordIndex]); // Append the next word to the sentence
+          setCurrentWordIndex((prevIndex) => prevIndex + 1); // Move to the next word
+        } else {
+          clearInterval(interval); // Stop the interval when done
+        }
+
+      }, 20); // Adjust delay as needed
+
+      return () => clearInterval(interval); // Cleanup the interval on component unmount
+    }
+  }, [currentWordIndex, paragraphText, words]);
+
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+    <div className="main-body">
       <h2>Page Paragraph</h2>
-      <p>{paragraphText}</p>
+      {/* <p>{paragraphText}</p><br /> */}
+      <p>{loader ? <MoonLoader size={40} color={'black'} /> : currentSentence}</p>
     </div>
   );
 }
 
 export default App;
-
